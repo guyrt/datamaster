daab95c66b1d783a03b9cf116d7ef24677968c71
Tommy Guy <riguy@microsoft.com>
2019-12-16 06:33:28 -0800

    moved some files

--- server/datamaster/syncer/tests/test_deletes.py
+++ server/datamaster/syncer/tests/test_deletes.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from django.test import Client, TestCase
 
 from .initializers import TestInitilizers
+from syncer.models import ClientDataSet
 
 
-class BranchTests(TestInitilizers):
+class BranchDeleteTests(TestInitilizers):
 
     def test_delete_branch_deletes(self):
         self.branch.deactivate()
@@ -12,3 +13,12 @@ class BranchTests(TestInitilizers):
         self.assertFalse(self.branch.is_active)
         self.cds.refresh_from_db()
         self.assertFalse(self.cds.is_active)
+
+
+class UserDeleteTest(TestInitilizers):
+
+    def test_delete_user_doesnt_delete(self):
+        self.user.delete()
+        local_cds = ClientDataSet.objects.get(id=self.cds.id)
+        self.assertTrue(local_cds.is_active)
+        self.assertIsNone(local_cds.user)
