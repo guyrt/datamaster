b34f2af1087663717680c6fd070100a234c8e16e
Tommy Guy <riguy@microsoft.com>
2020-01-11 14:34:58 -0800

    remove server code

--- server/datamaster/teams/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from teams import views
-
-
-urlpatterns = [
-    path('team/<int:team_pk>/user/', views.UserInTeamList.as_view(), name='users'),
-    path('user/<slug:username>/', views.UserDetails.as_view(), name='user-details'),
-]
